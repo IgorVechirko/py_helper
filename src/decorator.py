668b8b6ab1e7077a11e8d3b174a746184fa94f6d
IgorVechirko <vechirkoigor@gmail.com>
2023-06-22 22:32:54 +0300

    reset repo

--- src/decorator.py
+++
@@ -1,9 +0,0 @@
-def printFuncNameDecorator(func):
-	def wrapper(*args, **kwargs):
-		print('')
-		print('>>',func.__name__)
-		result = func(*args, **kwargs)
-		print('<<',func.__name__)
-		return result
-	
-	return wrapper
