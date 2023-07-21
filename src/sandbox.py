644a75d8e850dab93425edd7053ac3869094d271
Igor <vechirkoigor@gmail.com>
2023-07-21 19:05:16 +0300

    Add loops.py scritps

--- src/sandbox.py
+++ src/sandbox.py
@@ -1,19 +1,19 @@
 
-tuple_v1 = (547, "Python", True)
-list_v1 = [546, "I love python", tuple_v1]
-str_v1 = "Python is BS"
-range_v1 = range(39, -57, -6)
+var_v1 = True
+print(id(var_v1))
 
-#for tuple_elem in tuple_v1:
-#    print(tuple_elem)
+var_v1 = False
+print(id(var_v1))
 
-#for tuple_elem in range_v1:  # key word 'in' has not the same effect as word 'in' in this case '547 in tuple_v1'
-#    print(tuple_elem)
+var_v1 = True
+print(id(var_v1))
 
-concat_collection = list_v1 + list(tuple_v1)
-concat_collection_len = len(list_v1) + len(tuple_v1)
+var_v1 = False
+print(id(var_v1))
 
-for list_indx in range(0, concat_collection_len):
-    if list_indx % 2:
-        print(concat_collection[list_indx])
+list_v1 = [True, 54]
+list_v2 = list(list_v1)
+list_v2.append("Python")
 
+print(list_v1)
+print(list_v2)
