d6853a1c8cfb3738424a47099ac3a95fcbebe451
Igor <vechirkoigor@gmail.com>
2023-09-01 17:25:44 +0300

    finish functions samples

--- src/functions.py
+++ src/functions.py
@@ -26,13 +26,14 @@ default_args_func("Hello", default_arg1="Python", default_arg2="Buy buy")
 default_args_func("Hello", "Python", default_arg2="Buy buy")
 #default_args_func("Hello", default_arg1="Python", none_default_arg="Buy buy") danger none_default_arg specified twice
 
-vargs = ["Hello", "Python", "C++"]
-default_args_func(*vargs)
-kwargs = {"default_arg2": "Hello", "default_arg1": "Python", "none_default_arg": "C++"}
-default_args_func(**kwargs)
+args_list = ["Hello", "Python", "C++"]
+default_args_func(*args_list)
+args_dict = {"default_arg2": "Hello", "default_arg1": "Python", "none_default_arg": "C++"}
+default_args_func(**args_dict)
 
-
-
+args_list = ["Hello"]
+args_dict = {"default_arg1": "Python", "default_arg2": "Hello"}
+default_args_func(*args_list, **args_dict)
 
 #runtime_formed_default_val=input()
 #default_dict = {"2": 2, "19": 7}
@@ -44,13 +45,119 @@ def args_func(arg_1, arg_2, arg_3=1, arg_4={}):
 
     print(arg_1, arg_2, arg_3, arg_4)
 
-
 #args_func("Hello", "python", "C++", "Python")
 #args_func("Hello", "python")
 
-
-
-
-
 #default_val = input()
 #default_dict.setdefault("654", input())
+
+
+def function_as_variable(num):
+    print(num, "function-variable")
+
+
+value_returned_by_func = function_as_variable(1)
+holded_function_var = function_as_variable
+print(value_returned_by_func)
+print(holded_function_var)
+holded_function_var(2)
+
+print(id(function_as_variable), id(holded_function_var))
+print(type(function_as_variable), type(holded_function_var))
+
+function_as_variable = 1  # function name is firs-class sitizen so ye we can do this
+print(function_as_variable)
+#function_as_variable(3)
+
+
+lambda_val = lambda x, y: x + y
+print(lambda_val(4, 5))
+
+
+def func_with_nested_function(arg1, arg2):
+    def increment(val):
+        if val % 2:
+            return val + 1
+        else:
+            return val + 2
+
+    def decrement(val):
+        if val % 2:
+            return val - 1
+        else:
+            return val - 2
+
+    if arg1 + arg2 > 0:
+        return increment(arg1)
+    elif arg1 - arg2 > 0:
+        return increment(arg2)
+    elif arg2 - arg1 > 0:
+        return decrement(arg1)
+    else:
+        return decrement(arg2)
+
+
+print(func_with_nested_function(5, 6))
+
+
+def function_return_closures_state_inner_func(arg_var):
+    func_var = 20
+    def fun_with_closures_state(base):
+        print(arg_var)
+
+    return fun_with_closures_state
+
+closures_func1 = function_return_closures_state_inner_func(10)
+closures_func2 = function_return_closures_state_inner_func(50)
+closures_func1(30)
+closures_func2(30)
+
+
+def frame_func(func):
+    def exe(*vargs, **kwargs):
+        print("---------->", func.__name__, "<---------", sep="")
+        print()
+        func(*vargs, **kwargs)
+        print()
+        print("<----------", func.__name__, "---------->", sep="")
+        print()
+    return exe
+
+def cast_ret_to_str(func):
+    def cast(*vargs, **kwargs):
+        return str(func(*vargs, **kwargs)) + " str ret"
+    return cast
+
+
+@frame_func
+def try_function(arg1):
+    print("try_function", arg1)
+
+
+print(try_function("python"))
+
+
+
+def infinity_recursion(level):
+    print(level)
+    infinity_recursion(level + 1)
+
+
+#infinity_recursion(1)
+
+
+def factorial(n):
+
+    if n < 1:
+        return 0
+
+    if n == 1:
+        return n
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(10))
+
+
+
+
--- src/sandbox.py
+++ src/sandbox.py
@@ -1,16 +1,46 @@
 
-list_v1 = [5,3]
-list_v1.append(list_v1)
+def sort_coll(coll, pred):
+    i = 0
+    while i < len(coll)-1:
+        candidate_idx = -1
+        candidate = coll[i]
 
-print(list_v1)
+        j = i + 1
+        while j < len(coll):
+            if pred(coll[j], candidate):
+                candidate_idx = j
+                candidate = coll[j]
 
-list_copy = list_v1.copy()
-print(list_copy)
+            j += 1
+        else:
+            if candidate_idx != -1:
+                coll[i], coll[candidate_idx] = coll[candidate_idx], coll[i]
 
-def count_nested(list_arg):
-    for elem in list_arg:
-        if isinstance(elem, list):
-            return 1 + count_nested(elem)
+        i += 1
 
-print(count_nested(list_v1))
-print(count_nested(list_copy))
+
+def pred(elem1, elem2):
+    return elem1[1] < elem2[1]
+
+
+sortable = [("data", 5),
+            ("hello", 6),
+            ("world", 1),
+            ("python", 8),
+            ("C++", 54)]
+
+sort_coll(sortable, pred)
+print(sortable)
+
+
+
+
+
+def scoped_func(arg1, arg2):
+    ret = arg1 + arg2
+    print(ret)
+
+
+scoped_func(5, 6)
+
+print(arg1)
