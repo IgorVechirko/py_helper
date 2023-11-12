7551c31465466f3bd45c80c9136b5dd2b68362c7
Igor <vechirkoigor@gmail.com>
2023-11-12 10:12:00 +0200

    finish builin functions samples

--- src/builtin_funs.py
+++ src/builtin_funs.py
@@ -1,3 +1,6 @@
+import copy
+import sys
+
 def abs_builtin():
     print(abs(-3))
     print(abs(-3.6))
@@ -50,7 +53,82 @@ def hash_and_hex_builtins():
     print(hex(object_hash))
 
 
-def map_builtin():
-    pass
+def map_and_next_builtin():
+    numbers1 = [1, 2, 3, 4, 5]
+    numbers2 = [10, 20, 30, 40, 50]
+    numbers3 = [100, 200, 300, 400, 500]
+
+    iterator = map(lambda x, y, z: x + y + z, numbers1, numbers2, numbers3)
+
+    list_1 = list(iterator)
+    print(list_1)
+
+    iterator = map(lambda x, y, z: x + y + z, numbers1, numbers2, numbers3)
+
+    list_2 = []
+    try:
+        while(True):
+            list_2.append(next(iterator))
+    except StopIteration:
+        print(list_2)
+
+    iterator = map(lambda x, y, z: x + y + z, numbers1, numbers2, numbers3)
+    list_3 = []
+
+    value = next(iterator, StopIteration())
+    while not isinstance(value, StopIteration):
+        list_3.append(value)
+        value = next(iterator, StopIteration())
+
+    print(list_3)
+
+
+def max_min_builtin():
+    list_var = [4, 5, 3, 2, 54, 43, 5]
+
+    print(max(list_var))
+    print(min(4, 3, 5, 2, 0, 2, 5, 2))
+
+    try:
+        print(max([]))
+    except ValueError:
+        print("Empty iterable")
+
+    print(min([], default=None))
+
+    print(max((5, 3), (8, 3), (9, 1), (8, 4), (8, 3), key=lambda x: x[0]))
+
+
+def oct_builtin():
+    print(oct(378909))
+
+
+def repr_builtin():
+    print(list(repr("Hello python")))
+
+
+def reversed_builtin():
+    print("".join(reversed("Hello python")))
+
+
+def round_builtin():
+    print(round(7.837483654, 5))
+    print(round(7.837483654))
+
+
+def sorted_builtin():
+    raw_list = [54, 3, 6543, 3, 6, 2, 6, 343434, 63]
+
+    sorted_1 = sorted(raw_list)
+    sorted_2 = sorted(raw_list, reverse=True)
+    sorted_3 = sorted(raw_list, key=lambda x: -x)
+
+    print(sorted_1)
+    print(sorted_2)
+    print(sorted_3)
+
+
+def sum_builtin():
+    print(sum([54, 3, 6543, 3, 6, 2, 6, 343434, 63]))
 
 
