ac6c47c321515e8d6954f0407149917b51d49d56
Igor <vechirkoigor@gmail.com>
2023-12-26 14:53:02 +0200

    add saplesnofnusinf send, throw, close in generators

--- src/generators.py
+++ src/generators.py
@@ -22,4 +22,80 @@ def generator_function_sample():
         print(i)
 
 
-generator_function_sample()
+def generator_from_comprehension():
+    comprehension_generator = (i for i in [4, 5, 6, 3, 6])
+    print(next(comprehension_generator))
+    print(next(comprehension_generator))
+    print(next(comprehension_generator))
+    print(next(comprehension_generator))
+
+
+def send_value_in_to_generator():
+    def generator_func():
+        val = 5
+        do_work = True
+        while True:
+            if do_work:
+                val *= val
+
+            do_work = (yield val)
+
+    big_num = 654320
+    gen_obj = generator_func()
+
+    gen_val = next(gen_obj)
+    while gen_val < big_num:
+        print(gen_val)
+        gen_val = gen_obj.send(True)
+
+    next(gen_obj)
+
+
+def throw_except_in_to_generator():
+    def generator_func():
+        val = 5
+        do_work = True
+        while True:
+            if do_work:
+                val *= val
+
+            do_work = (yield val)
+
+    big_num = 654320
+    gen_obj = generator_func()
+
+    gen_val = next(gen_obj)
+    while gen_val < big_num:
+        print(gen_val)
+        gen_val = gen_obj.send(True)
+
+        if(len(str(gen_val)) >= 5):
+            gen_obj.throw("We don't like large numbers")
+
+    next(gen_obj)
+
+
+def close_generator():
+    def generator_func():
+        val = 5
+        do_work = True
+        while True:
+            if do_work:
+                val *= val
+
+            do_work = (yield val)
+
+    big_num = 654320
+    gen_obj = generator_func()
+
+    gen_val = next(gen_obj)
+    while gen_val < big_num:
+        print(gen_val)
+        gen_val = gen_obj.send(True)
+    else:
+        gen_obj.close()
+
+    next(gen_obj)
+
+
+close_generator()
